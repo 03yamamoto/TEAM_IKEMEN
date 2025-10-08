d68e98ef657c010d8d8a4e302d4fa4c5d97e6fc2
user11 <intecs.2506ap.11@gmail.com>
2025-10-08 15:26:58 +0900

    UserBean完成

--- src/main/java/jp/co/seminar/beans/UserBean.java
+++ src/main/java/jp/co/seminar/beans/UserBean.java
@@ -1,5 +1,48 @@
 package jp.co.seminar.beans;
 
-public class UserBean {
+import java.io.Serializable;
+
+public class UserBean implements Serializable {
+
+	//フィールド
+	private static final long serialVersionUID = 1L;
+	private String address;
+	private String id;
+	private String name;
+	private String password;
+
+	//コンストラクタ
+	UserBean() {
+	}
+
+	UserBean(String id, String password, String name, String address) {
+		id = id;
+		password = password;
+		name = name;
+		address = address;
+	}
+
+	//メソッド
+	public String getAddress() {
+		return address;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	//デバッグ用ToString
+	@Override
+	public String toString() {
+		return "アドレス:" + address + "ID:" + id + "名前:" + name + "パスワード:" + password;
+	}
 
 }
