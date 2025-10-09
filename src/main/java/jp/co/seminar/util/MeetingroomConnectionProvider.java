1719691734cae1690efe2ca1a57a73c1d066b037
yamamoto <intecs.2506ap.03@gmail.com>
2025-10-09 09:41:26 +0900

    ドライバ追加

---
+++ src/main/java/jp/co/seminar/util/MeetingroomConnectionProvider.java
@@ -0,0 +1,8 @@
+package jp.co.seminar.util;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class MeetingroomConnectionProvider implements DatabaseConfig {
+	public static Connection getConnection() throws ClassNotFoundException, SQLException{}
+}
